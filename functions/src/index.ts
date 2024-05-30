d630b94e8e7b1fca42b4d39ea7d0500c9422f6bb
alvaro <alvaro.bonilla.lledo@gmail.com>
2024-05-30 19:27:33 +0200

    firebase & mongodb conected

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import cors from "cors";
 import userRouter from "./routes/userRoutes";
 import categoryRouter from "./routes/categoryRoutes";
@@ -9,17 +10,19 @@ dotenv.config();
 
 const app = express();
 
-const corsOptions = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-  origin: true
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Configuración
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+mongoose.connect(process.env.MONGODB_URI as string)
+  .then(() => {
+    console.log("Conexión a MongoDB establecida");
+  })
+  .catch((error) => {
+    console.error("Error al conectar a MongoDB:", error);
+  });
 
 // Rutas
 app.use("/user", userRouter);
